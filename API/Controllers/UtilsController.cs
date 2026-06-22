d2a048ed2304b9a7a58f23638dd4c0ad68e61106
Monfikowsky <bartek.mikolaj.wloszek@wp.pl>
2026-06-22 19:51:53 +0200

    Now building image from ghcr and created version endpoint

---
+++ API/Controllers/UtilsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UtilsController : ControllerBase
+    {
+        [HttpGet("version")]
+        public IActionResult Version()
+        {
+            return Ok("v2");
+        }
+    }
+}
